e5898592fc1bf048258443429e2eb0d24872fbc9
Wairimu2018 <wairimukuria.wk@gmail.com>
2023-03-11 20:44:09 +0300

    App code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
+import { Toaster } from "react-hot-toast";
+import { useDispatch } from "react-redux";
+import { RouterProvider } from "react-router-dom";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      dispatch(getUser(user.email))
+    }
+  })
+}, [dispatch]
+  
+return (
+  <div className="px-6">
+    <Toaster />
+    <RouterProvider router={routes} />
+    <div className="grid grid-cols-2">
+      <div></div>
+      <div></div>
     </div>
-  );
-}
+  </div>
+);
+} 
 
 export default App;
